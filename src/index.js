55c04400ff61f95e876b1244aef90099e284a19c
Tran Tuan Kiet <contact.trtkiet@gmail.com>
2024-11-22 14:12:04 +0700

    update env

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const { engine } = require ('express-handlebars');
+const app = express();
+const port = 3000;
+
+//Template engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
